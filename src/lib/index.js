25ee3124f8b241a2e4480f3011a97f62f185784a
Paul Zimmer <mrzmmr@gmail.com>
2016-01-24 02:17:11 -0800

    Add badge

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,8 @@
 /**
+ * [![AppVeyor branch](https://img.shields.io/appveyor/ci/mrzmmr/defop/master.svg?style=flat-square)]()
+ * [![CircleCI branch](https://img.shields.io/circleci/project/mrzmmr/defop/master.svg?style=flat-square)]()
+ * [![Coveralls branch](https://img.shields.io/coveralls/mrzmmr/defop/master.svg?style=flat-square)]()
+ *
  * ## install
  *
  * ```sh
